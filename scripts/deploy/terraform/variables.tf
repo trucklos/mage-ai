81d60f46c382abac8d9635c3f01d080705e55a3c
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-09-06 14:27:15 -0700

    [xy] Update image.

--- scripts/deploy/terraform/variables.tf
+++ scripts/deploy/terraform/variables.tf
@@ -34,7 +34,7 @@ variable "cidr" {
 
 variable "docker_image" {
   description = "Docker image url used in ECS task."
-  default     = "mageai/mageai-event-trigger-test:with-event-trigger"
+  default     = "mageai/mageai-orchestration-ui:latest"
 }
 
 variable "ecs_task_cpu" {
